c49021b421a2d87d7e15eb15b81420334619d726
Andika <Andika@LAPTOP-PNGP4BPM>
2022-02-09 14:45:07 +0700

    Add project files.

---
+++ Customer/Consumer-cmd.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using RabbitMQ
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class RMQ
+{
+    public ConnectionFactory connectionFactory;
+    public IConnection connection;
+    public IModel channel;
+    private bool isReceiving = false;
+    public void InitRMQConnection(string host = "localhost", int port = 5672, string user =
+    "guest", string pass = "guest")
+    {
+        connectionFactory = new ConnectionFactory();
+        connectionFactory.HostName = host;
+        //connectionFactory.Port = port;
+        connectionFactory.UserName = user;
+        connectionFactory.Password = pass;
+    }
+    public void CreateRMQConnection()
+    {
+        connection = connectionFactory.CreateConnection();
+        Console.WriteLine("Koneksi " + (connection.IsOpen ? "Berhasil!" : "Gagal!"));
+    }
+    public void WaitingMessage(string queue_name)
+    {
+        using (channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: queue_name,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Pesan diterima: {0}", message);
+            };
+            channel.BasicConsume(queue: queue_name,
+            autoAck: true,
+            consumer: consumer);
+            Console.WriteLine(" Tekan [enter] untuk memutus koneksi.");
+            Console.ReadLine();
+            Disconnect();
+        }
+    }
+    public void Disconnect()
+    {
+        isReceiving = false;
+        channel.Close();
+        channel = null;
+        Console.WriteLine("Channel ditutup!");
+        if (connection.IsOpen)
+        {
+            connection.Close();
+        }
+        Console.WriteLine("Koneksi diputus!");
+        connection.Dispose();
+        connection = null;
+    }
+}
---
+++ Customer/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+RMQ rmq = new RMQ();
+Console.WriteLine("Tekan tombol apapun untuk inisialisasi RMQ parameters.");
+Console.ReadKey();
+rmq.InitRMQConnection(); // inisialisasi parameter (secara default) untuk koneksi ke server RMQ
+Console.WriteLine("Tekan tombol apapun untuk membuka koneksi ke RMQ.");
+Console.ReadKey();
+rmq.CreateRMQConnection(); // memulai koneksi dengan RMQ
+Console.Write("Masukkan nama queue channel untuk menerima pesan melalui RMQ.\n>> ");
+string queue_name = Console.ReadLine();
+Console.WriteLine("Menunggu pesan masuk...");
+rmq.WaitingMessage(queue_name);
